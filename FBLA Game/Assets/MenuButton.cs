45e80c6b71226531109f9e8009aaff0922292126
Tajvir Chahal <tajvirchahal87@gmail.com>
2019-09-15 08:58:15 -0400

    Created menu script
    -Script that transitions to next scene, when clicking the start button.
    -Script also exits game on quit
    
    NOTE: I added used a method to exit the editor, but we'll have to remove that later

---
+++ FBLA Game/Assets/MenuButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    public void StartFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false; //This temporary, just to stop game in editor
+        Application.Quit();
+    }
+}
+
