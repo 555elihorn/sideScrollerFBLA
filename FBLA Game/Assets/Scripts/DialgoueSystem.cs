dacc596b5162929605a850bed3e81f01c54dec90
Tajvir C <tajvirchahal87@gmail.com>
2019-09-22 19:34:44 -0400

    Added Basic Dialogue Script
    When the player collides with the NPC Trigger. The Dialogue Script, will display the text conversation and a continue button to the next line of dialogue.

--- FBLA Game/Assets/Scripts/DialgoueSystem.cs
+++ FBLA Game/Assets/Scripts/DialgoueSystem.cs
@@ -1,31 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Threading;
 
 public class DialgoueSystem : MonoBehaviour
 {
 
     bool playerIsTouching = false;
 
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+
+    [SerializeField] float typingSpeed = 0.02f;
+    public GameObject continueButton;
+
+
 
     void Start()
     {
-        
+        continueButton.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(playerIsTouching)
+        //continueButton.SetActive(true);
+    }
+
+    IEnumerator Type()
+    {
+        string currentString = sentences[index];
+        print(currentString);
+
+        foreach (char letter in currentString)
         {
-            print("Nice");
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed); //Typing speed on text
+
+        }
+
+    }
+
+    public void NextSentence()
+    {
+        continueButton.SetActive(true);
+
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+            continueButton.SetActive(false);
         }
     }
+
+ 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
         if (collision.gameObject.name == "Player")
         {
-            playerIsTouching = true;
+            continueButton.SetActive(true);
+            StartCoroutine(Type());
         }
 
     }
@@ -34,7 +76,11 @@ public class DialgoueSystem : MonoBehaviour
     {
         if (collision.gameObject.name == "Player")
         {
-            playerIsTouching = false;
+            print("FOUND");
+            continueButton.SetActive(false);
+            textDisplay.text = "";
+            index = 0;
+            StopCoroutine(Type());
         }
     }
 }
