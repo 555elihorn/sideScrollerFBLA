f5e26eb26da3e88d46deb3ae7de3c42db22b6612
Tajvir C <tajvirchahal87@gmail.com>
2019-09-22 10:44:19 -0400

    Added NPC User Detection System
    Whenever a player gets within a range of an npc, the trigger collider will detect them.

---
+++ FBLA Game/Assets/Scripts/DialgoueSystem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialgoueSystem : MonoBehaviour
+{
+
+    bool playerIsTouching = false;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerIsTouching)
+        {
+            print("Nice");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            playerIsTouching = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            playerIsTouching = false;
+        }
+    }
+}
