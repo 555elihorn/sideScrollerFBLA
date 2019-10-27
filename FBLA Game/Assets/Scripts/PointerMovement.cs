dd6a2cba4f6715c9164e7ace2acbffdfca76ec5f
Tajvir C <tajvirchahal87@gmail.com>
2019-10-27 14:59:07 -0400

    Finished Pointer Movement Script
    Whenever the pointer collides with the end of the bar, the arrow moves in the other direction

--- FBLA Game/Assets/Scripts/PointerMovement.cs
+++ FBLA Game/Assets/Scripts/PointerMovement.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class PointerMovement : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D myRigidBody;
+
+    bool isTouchingEnd = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        myRigidBody.velocity = new Vector2(moveSpeed, 0f);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("CHECK");
+        moveSpeed *= -1;
     }
 }
