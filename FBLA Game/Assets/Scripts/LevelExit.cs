ebdb85eb5cc34dff0082ea10ea20e60fb569b27a
Tajvir C <tajvirchahal87@gmail.com>
2019-11-09 14:31:58 -0500

    Added Level Exit script

---
+++ FBLA Game/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+
+    [SerializeField] float LevelLoadDelay = 1f;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(LevelLoadDelay);
+
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+}
