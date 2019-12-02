d830caeea1501ef32bece7d86f01f02308093f67
Tajvir C <tajvirchahal87@gmail.com>
2019-12-02 12:14:38 -0500

    Workings on slides script

---
+++ FBLA Game/Assets/Slides.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Slides : MonoBehaviour
+{
+
+    //config
+    [SerializeField] int currentslide = 1;
+    [SerializeField] GameObject[] slides;
+    [SerializeField] TextMeshProUGUI slideCounterText = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slideCounterText.text = slideCounterText.ToString() + " / " + slides.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
