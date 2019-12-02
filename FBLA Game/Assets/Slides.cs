45bdb1ad3aebedc4d7bcfb228ae7d8a9e6381bf0
Tajvir C <tajvirchahal87@gmail.com>
2019-12-02 12:35:23 -0500

    Created basic skeleton methods to forward slides

--- FBLA Game/Assets/Slides.cs
+++ FBLA Game/Assets/Slides.cs
@@ -6,20 +6,44 @@ using UnityEngine;
 public class Slides : MonoBehaviour
 {
 
+    //variables
+    int currentslide = 0;
+
     //config
-    [SerializeField] int currentslide = 1;
     [SerializeField] Sprite[] slides;
     [SerializeField] TextMeshProUGUI slideCounterText = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        slideCounterText.text = currentslide + " / " + slides.Length;
+        slideCounterText.text = (currentslide + 1) + " / " + slides.Length;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void ForwardSlides()
+    {
+        if(currentslide != slides.Length - 1)
+        {
+            print("Forwarding!");
+            currentslide++;
+            slideCounterText.text = (currentslide + 1) + " / " + slides.Length;
+            return;
+        }
+    }
+
+    public void ReverseSlides()
+    {
+        if(currentslide != 0)
+        {
+            print("REVERSING");
+            currentslide--;
+            slideCounterText.text = (currentslide + 1) + " / " + slides.Length;
+            return;
+        }
     }
 }
