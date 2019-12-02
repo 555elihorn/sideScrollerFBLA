be144ba405c6c529f2c6c0e29f28a02ba77f14ab
Tajvir C <tajvirchahal87@gmail.com>
2019-12-02 12:23:28 -0500

    Switched arrows images to buttons

--- FBLA Game/Assets/Slides.cs
+++ FBLA Game/Assets/Slides.cs
@@ -8,13 +8,13 @@ public class Slides : MonoBehaviour
 
     //config
     [SerializeField] int currentslide = 1;
-    [SerializeField] GameObject[] slides;
+    [SerializeField] Sprite[] slides;
     [SerializeField] TextMeshProUGUI slideCounterText = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        slideCounterText.text = slideCounterText.ToString() + " / " + slides.Length;
+        slideCounterText.text = currentslide + " / " + slides.Length;
     }
 
     // Update is called once per frame
