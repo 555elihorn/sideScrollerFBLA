9f9077e04ed8fbf79d38e255980646b05bc7eb0f
Tajvir C <tajvirchahal87@gmail.com>
2019-12-02 12:51:35 -0500

    Finished slides script
    -Added helper method that converts sprites to textures
    -Script now works properly

--- FBLA Game/Assets/Slides.cs
+++ FBLA Game/Assets/Slides.cs
@@ -2,48 +2,79 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Slides : MonoBehaviour
 {
 
     //variables
-    int currentslide = 0;
+    int currentSlide = 0;
+    
 
     //config
     [SerializeField] Sprite[] slides;
+    [SerializeField] GameObject display;
     [SerializeField] TextMeshProUGUI slideCounterText = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        slideCounterText.text = (currentslide + 1) + " / " + slides.Length;
+        slideCounterText.text = (currentSlide + 1) + " / " + slides.Length;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     public void ForwardSlides()
     {
-        if(currentslide != slides.Length - 1)
+        
+
+        if (currentSlide != slides.Length - 1)
         {
             print("Forwarding!");
-            currentslide++;
-            slideCounterText.text = (currentslide + 1) + " / " + slides.Length;
+            currentSlide++;
+            slideCounterText.text = (currentSlide + 1) + " / " + slides.Length;
+
+            var img = display.GetComponent<RawImage>();
+            img.texture = textureFromSprite(slides[currentSlide]);
+
             return;
         }
     }
 
     public void ReverseSlides()
     {
-        if(currentslide != 0)
+        if(currentSlide != 0)
         {
             print("REVERSING");
-            currentslide--;
-            slideCounterText.text = (currentslide + 1) + " / " + slides.Length;
+            currentSlide--;
+            slideCounterText.text = (currentSlide + 1) + " / " + slides.Length;
+
+            var img = display.GetComponent<RawImage>();
+            img.texture = textureFromSprite(slides[currentSlide]);
+
             return;
         }
+    }
+
+    //helper method that converts sprite to texture
+    public static Texture2D textureFromSprite(Sprite sprite)
+    {
+        if (sprite.rect.width != sprite.texture.width)
+        {
+            Texture2D newText = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
+            Color[] newColors = sprite.texture.GetPixels((int)sprite.textureRect.x,
+                                                         (int)sprite.textureRect.y,
+                                                         (int)sprite.textureRect.width,
+                                                         (int)sprite.textureRect.height);
+            newText.SetPixels(newColors);
+            newText.Apply();
+            return newText;
+        }
+        else
+            return sprite.texture;
     }
 }
