ff116076cee75a208b8962eca0d8e5ee4df11787
Elijah Horn <555elihorn@gmail.com>
2020-02-05 12:45:11 -0500

    Added ai script, capsule collider

---
+++ FBLA Game/Assets/EnemyAI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
