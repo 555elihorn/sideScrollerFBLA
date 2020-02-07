8ea0071a9471bf322e8d4263faad8b918fcaae13
Elijah Horn <555elihorn@gmail.com>
2020-02-07 12:36:49 -0500

    Enemy AI script
    needs testing

--- FBLA Game/Assets/EnemyAI.cs
+++ FBLA Game/Assets/EnemyAI.cs
@@ -6,7 +6,9 @@ public class EnemyAI : MonoBehaviour
 {
     // Start is called before the first frame update
     private Rigidbody2D rb2d;
-
+    private bool ballIsIn = false;
+    public float speed = 10.0f;
+    public float boundY = 2.25f;
     [SerializeField] GameObject ball;
     void Start()
     {
@@ -16,14 +18,41 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var vel = rb2d.velocity;
+        if (ballIsIn)
+        {
+           if(ball.transform.position.y > rb2d.transform.position.y)
+            {
+                vel.y = speed;
+            } 
+            else if (ball.transform.position.y < rb2d.transform.position.y)
+            {
+                vel.y = -speed;
+            }
+        }
+        else
+        {
+            vel.y = 0;
+        }
+
+        var pos = rb2d.transform.position;
+
+        if (pos.y > boundY)
+        {
+            pos.y = boundY;
+        }
+        else if (pos.y < -boundY)
+        {
+            pos.y = -boundY;
+        }
+        transform.position = pos;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag=="Ball")
         {
-
+            ballIsIn = true;
         }
     }
 }
