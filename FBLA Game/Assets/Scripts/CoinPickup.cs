9c9f55ec2b3c441e34b85d3c5426232b081dfda4
Tajvir C <tajvirchahal87@gmail.com>
2020-03-21 12:49:56 -0400

    Added checkpoint sprites and working on checkpoint script

--- FBLA Game/Assets/Scripts/CoinPickup.cs
+++ FBLA Game/Assets/Scripts/CoinPickup.cs
@@ -12,7 +12,7 @@ public class CoinPickup : MonoBehaviour
     //On trigger if the player collides with the coin; remove the coin and add to score
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.ToString().Contains("Capsule"))
+        if (collision.gameObject.tag.Equals("Player"))
         {
             FindObjectOfType<GameSession>().AddToScore(pointsForCoinPickup);
             AudioSource.PlayClipAtPoint(coinPickUpSFX, Camera.main.transform.position, 0.5f);
